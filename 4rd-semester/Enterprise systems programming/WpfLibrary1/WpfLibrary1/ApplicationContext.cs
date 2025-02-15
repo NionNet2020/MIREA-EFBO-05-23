9d0f3697b78b932d9fb4d80ec496ff9f6f4268ee
Nomos <nikonet2015@gmail.com>
2025-02-15 18:24:54 +0300

    Upload 4 sem

---
+++ 4rd-semester/Enterprise systems programming/WpfLibrary1/WpfLibrary1/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SQLiteApp
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+}
